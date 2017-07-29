78ca33e81e23ae14efae8b6aadb622176f730356
Sand Moshi <sandmoshi@gmail.com>
2017-07-28 20:53:01 -0600

    Changed sample plays to be x100 and y100

--- app/src/sample-plays.js
+++ app/src/sample-plays.js
@@ -5,11 +5,11 @@ const plays = {
     desc: 'The ball is passed to each player in the backline from 10 to winger.',
     video: '',
     items:{
-      "drawing-1500056996575":{"h":64,"src":"jersey 10","tool":"icon","w":64,"x":305,"y":200.125},
-      "drawing-1500057019808":{"h":64,"src":"jersey 12","tool":"icon","w":64,"x":375,"y":240.125},
-      "drawing-1500057024353":{"h":64,"src":"jersey 13","tool":"icon","w":64,"x":435,"y":280.125},
-      "drawing-1500057044102":{"h":64,"src":"jersey 15","tool":"icon","w":64,"x":495,"y":320.125},
-      "drawing-1500057040279":{"h":64,"src":"jersey 14","tool":"icon","w":64,"x":555,"y":350.125},
+      "drawing-1500056996575":{"h":64,"src":"jersey 10","tool":"icon","w":64,"x":0.3803,"y":0.333},
+      "drawing-1500057019808":{"h":64,"src":"jersey 12","tool":"icon","w":64,"x":0.469,"y":0.4},
+      "drawing-1500057024353":{"h":64,"src":"jersey 13","tool":"icon","w":64,"x":0.544,"y":0.467},
+      "drawing-1500057044102":{"h":64,"src":"jersey 15","tool":"icon","w":64,"x":0.619,"y":0.533},
+      "drawing-1500057040279":{"h":64,"src":"jersey 14","tool":"icon","w":64,"x":0.694,"y":0.583},
     },
   },
 
@@ -19,11 +19,11 @@ const plays = {
     desc: 'The flyhalf (10) angles his run in order to cross in front of his inside center (12) who will receive the ball as they cross.',
     video: '',
     items: {
-      "drawing-1500230536152":{"h":64,"src":"jersey 10","tool":"icon","w":64,"x":297,"y":256.125},
-      "drawing-1500230540762":{"h":64,"src":"jersey 12","tool":"icon","w":64,"x":334,"y":298.125},
-      "drawing-1500230550216":{"h":64,"src":"jersey 13","tool":"icon","w":64,"x":412,"y":324.125},
-      "drawing-1500230557635":{"h":64,"src":"jersey 15","tool":"icon","w":64,"x":488,"y":365.125},
-      "drawing-1500230567998":{"h":64,"src":"jersey 14","tool":"icon","w":64,"x":569,"y":370.125},
+      "drawing-1500230536152":{"h":64,"src":"jersey 10","tool":"icon","w":64,"x":0.371,"y":0.427},
+      "drawing-1500230540762":{"h":64,"src":"jersey 12","tool":"icon","w":64,"x":0.418,"y":0.497},
+      "drawing-1500230550216":{"h":64,"src":"jersey 13","tool":"icon","w":64,"x":0.515,"y":0.540},
+      "drawing-1500230557635":{"h":64,"src":"jersey 15","tool":"icon","w":64,"x":0.610,"y":0.608},
+      "drawing-1500230567998":{"h":64,"src":"jersey 14","tool":"icon","w":64,"x":0.711,"y":0.617},
     }
   },
 
@@ -33,11 +33,11 @@ const plays = {
       desc: 'The inside-center (12) runs a line as if he was receiving the ball and the flyhalf(10) instead passes directly to the outside-center(13).',
       video: '',
       items: {
-        "drawing-1500231153615":{"h":64,"src":"jersey 10","tool":"icon","w":64,"x":309,"y":252.125},
-        "drawing-1500231158391":{"h":64,"src":"jersey 12","tool":"icon","w":64,"x":362,"y":244.125},
-        "drawing-1500231163197":{"h":64,"src":"jersey 13","tool":"icon","w":64,"x":413,"y":305.125},
-        "drawing-1500231168626":{"h":64,"src":"jersey 15","tool":"icon","w":64,"x":487,"y":336.125},
-        "drawing-1500231174840":{"h":64,"src":"jersey 14","tool":"icon","w":64,"x":565,"y":346.125},
+        "drawing-1500231153615":{"h":64,"src":"jersey 10","tool":"icon","w":64,"x":0.386,"y":0.420},
+        "drawing-1500231158391":{"h":64,"src":"jersey 12","tool":"icon","w":64,"x":0.453,"y":0.407},
+        "drawing-1500231163197":{"h":64,"src":"jersey 13","tool":"icon","w":64,"x":0.516,"y":0.508},
+        "drawing-1500231168626":{"h":64,"src":"jersey 15","tool":"icon","w":64,"x":0.609,"y":0.560},
+        "drawing-1500231174840":{"h":64,"src":"jersey 14","tool":"icon","w":64,"x":0.706,"y":0.577},
       }
   },
 };
